ae450306c7ba401556d9d8e3802a7a7e22551298
Shobhit <shobhitkumar@illuminz.com>
2017-04-11 14:42:34 +0530

    add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
 var socket = io();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function(){
 	console.log('connected to socket.io server');
@@ -6,20 +10,25 @@ socket.on('connect', function(){
 
 socket.on('message', function(message){
 	var momentTimestamp = moment.utc(message.timestamp);
+	var $message = jQuery('.messages');
 	console.log('New message');
 	console.log(message.text);
-	jQuery('.messages').append('<p><strong>'+momentTimestamp.local().format('h:mm:a')+' - </strong>'+message.text+ '</p>');
+
+	$message.append('<p><strong>'+ message.name+ ' ' + momentTimestamp.local().format('h:mm:a') +'</strong></p>')
+	//jQuery('.messages').append('<p><strong>'+momentTimestamp.local().format('h:mm:a')+' - </strong>'+message.text+ '</p>');
+	$message.append('<p>'+message.text+'</p>')
 });
 //handle submitting a new msg
 var $form = jQuery('#message-form');
 
 $form.on('submit', function(event){
-	event.preventDefault();
+	event.preventDefault(); 
 
 	var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
-	 text:  $message.val()
+	  name:name,
+	  text:  $message.val()
 	});
 	
 	$message.val('');
